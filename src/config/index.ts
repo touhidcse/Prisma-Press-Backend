6099c3b3bbeaa8d283e28a3af70ad50d9a71ee98
touhidcse <touhidcucse38@gmail.com>
2026-07-08 01:09:51 +0600

    seting up strip Webhhok Endpoint API route for Subscription

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,8 +10,9 @@ export default {
     bcrypt_salt_rounds : process.env.BCRYPT_SALT_ROUNDS,
     jwt_access_secret: process.env.JWT_ACCESS_SECRET!,
     jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
-    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
-    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
+    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
     stripe_product_price_id: process.env.STRIPE_PRODUCT_PRICE_ID!,
-    stripe_secret_key: process.env.STRIPE_SECRET_KEY,
+    stripe_secret_key: process.env.STRIPE_SECRET_KEY!,
+    stripe_webhook_secret : process.env.STRIPE_WEBHOOK_SECRET!,
 }
